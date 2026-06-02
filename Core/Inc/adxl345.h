c7b8c11ab165438d07da4987eb9f030b58d33bde
eylloztek <eyluloztek2@gmail.com>
2026-06-02 14:50:15 +0300

    standby mode, measure mode, roll-pitch calculating

--- Core/Inc/adxl345.h
+++ Core/Inc/adxl345.h
@@ -8,112 +8,135 @@
 #ifndef INC_ADXL345_H_
 #define INC_ADXL345_H_
 
-#include "main.h"
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
 
-#define ADXL345_DEVICE_ADDRESS 		0xA6
+#define ADXL345_ADDRESS_LOW        			(0x53 << 1)
+#define ADXL345_ADDRESS_HIGH       			(0x1D << 1)
+#define ADXL345_DEVICE_ID          			0xE5
 
-#define DEVID 						0x00
-#define THRESH_TAP 					0x1D
-#define OFSX						0x1E
-#define OFSY						0x1F
-#define OFSZ						0x20
-#define DUR							0x21
-#define LATENT						0x22
-#define WINDOW						0x23
-#define THRESH_ACT					0x24
-#define THRESH_INACT				0x25
-#define TIME_INACT					0x26
-#define ACT_INACT_CTL				0x27
-#define THRESH_FF					0x28
-#define TIME_FF 					0x29
-#define TAP_AXES					0x2A
-#define ACT_TAP_STATUS				0x2B
-#define BW_RATE						0x2C
-#define POWER_CTL					0x2D
-#define INT_ENABLE 					0x2E
-#define INT_MAP						0x2F
-#define INT_SOURCE					0x30
-#define DATA_FORMAT					0x31
-#define DATAX0 						0x32
-#define DATAX1						0x33
-#define DATAY0						0x34
-#define DATAY1 						0x35
-#define DATAZ0						0x36
-#define DATAZ1						0x37
-#define FIFO_CTL					0x38
-#define FIFO_STATUS					0x39
+#define ADXL345_DEVID 						0x00
+#define ADXL345_THRESH_TAP 					0x1D
+#define ADXL345_OFSX						0x1E
+#define ADXL345_OFSY						0x1F
+#define ADXL345_OFSZ						0x20
+#define ADXL345_DUR							0x21
+#define ADXL345_LATENT						0x22
+#define ADXL345_WINDOW						0x23
+#define ADXL345_THRESH_ACT					0x24
+#define ADXL345_THRESH_INACT				0x25
+#define ADXL345_TIME_INACT					0x26
+#define ADXL345_ACT_INACT_CTL				0x27
+#define ADXL345_THRESH_FF					0x28
+#define ADXL345_TIME_FF 					0x29
+#define ADXL345_TAP_AXES					0x2A
+#define ADXL345_ACT_TAP_STATUS				0x2B
+#define ADXL345_BW_RATE						0x2C
+#define ADXL345_POWER_CTL					0x2D
+#define ADXL345_INT_ENABLE 					0x2E
+#define ADXL345_INT_MAP						0x2F
+#define ADXL345_INT_SOURCE					0x30
+#define ADXL345_DATA_FORMAT					0x31
+#define ADXL345_DATAX0 						0x32
+#define ADXL345_DATAX1						0x33
+#define ADXL345_DATAY0						0x34
+#define ADXL345_DATAY1 						0x35
+#define ADXL345_DATAZ0						0x36
+#define ADXL345_DATAZ1						0x37
+#define ADXL345_FIFO_CTL					0x38
+#define ADXL345_FIFO_STATUS					0x39
 
-#define TIMEOUT						1000
+#define ADXL345_TIMEOUT						1000
 
-#define X							DATAX0
-#define Y							DATAY0
-#define Z 							DATAZ0
+#define ADXL345_SCALE_FACTOR_FULL_RES   	(float)1/256.0
+#define ADXL345_SCALE_FACTOR_2G 			(float)1/256.0
+#define ADXL345_SCALE_FACTOR_4G 			(float)1/128.0
+#define ADXL345_SCALE_FACTOR_8G 			(float)1/64.0
+#define ADXL345_SCALE_FACTOR_16G 			(float)1/32.0
 
-#define WAKEUP_8HZ					0x00
-#define WAKEUP_4HZ					0x01
-#define WAKEUP_2HZ					0x02
-#define WAKEUP_1HZ					0x03
+#define ADXL345_WAKEUP_8HZ					0x00
+#define ADXL345_WAKEUP_4HZ					0x01
+#define ADXL345_WAKEUP_2HZ					0x02
+#define ADXL345_WAKEUP_1HZ					0x03
 
-#define RANGE_2G					0x00
-#define RANGE_4G					0x01
-#define RANGE_8G					0x02
-#define RANGE_16G					0x03
+#define ADXL345_RANGE_2G					0x00
+#define ADXL345_RANGE_4G					0x01
+#define ADXL345_RANGE_8G					0x02
+#define ADXL345_RANGE_16G					0x03
 
-#define BANDWIDTH_RATE_1600			0x0F
-#define BANDWIDTH_RATE_800			0x0E
-#define BANDWIDTH_RATE_400			0x0D
-#define BANDWIDTH_RATE_200			0x0C
-#define BANDWIDTH_RATE_100			0x0B
-#define BANDWIDTH_RATE_50			0x0A
-#define BANDWIDTH_RATE_25			0x09
+#define ADXL345_DATARATE_3200HZ  			0x0F
+#define ADXL345_DATARATE_1600HZ  			0x0E
+#define ADXL345_DATARATE_800HZ   			0x0D
+#define ADXL345_DATARATE_400HZ   			0x0C
+#define ADXL345_DATARATE_200HZ   			0x0B
+#define ADXL345_DATARATE_100HZ   			0x0A
+#define ADXL345_DATARATE_50HZ    			0x09
+#define ADXL345_DATARATE_25HZ    			0x08
+#define ADXL345_DATARATE_12_5HZ  			0x07
+#define ADXL345_DATARATE_6_25HZ  			0x06
+#define ADXL345_DATARATE_3_13HZ  			0x05
+#define ADXL345_DATARATE_1_56HZ  			0x04
+#define ADXL345_DATARATE_0_78HZ  			0x03
+#define ADXL345_DATARATE_0_39HZ  			0x02
+#define ADXL345_DATARATE_0_20HZ  			0x01
+#define ADXL345_DATARATE_0_10HZ  			0x00
 
-typedef enum{
-	READ_FAIL = 0,
-	READ_SUCCESS = 1
-}ADXL345ReadStatus;
+/* POWER_CTL Register Bits */
+#define ADXL345_POWER_CTL_WAKEUP_MASK       0x03
+#define ADXL345_POWER_CTL_SLEEP             (1 << 2)
+#define ADXL345_POWER_CTL_MEASURE           (1 << 3)
+#define ADXL345_POWER_CTL_AUTO_SLEEP        (1 << 4)
+#define ADXL345_POWER_CTL_LINK              (1 << 5)
 
-typedef enum{
-	WRITE_FAIL = 0,
-	WRITE_SUCCESS = 1
-}ADXL345WriteStatus;
+/* DATA_FORMAT Register Bits */
+#define ADXL345_DATA_FORMAT_RANGE_MASK      0x03
+#define ADXL345_DATA_FORMAT_JUSTIFY         (1 << 2)
+#define ADXL345_DATA_FORMAT_FULL_RES        (1 << 3)
+#define ADXL345_DATA_FORMAT_INT_INVERT      (1 << 5)
+#define ADXL345_DATA_FORMAT_SPI             (1 << 6)
+#define ADXL345_DATA_FORMAT_SELF_TEST       (1 << 7)
 
-typedef enum{
-	INIT_FAIL = 0,
-	INIT_SUCCESS = 1
-}ADXL345InitStatus;
+/* BW_RATE Register Bits */
+#define ADXL345_BW_RATE_RATE_MASK           0x0F
+#define ADXL345_BW_RATE_LOW_POWER           (1 << 4)
 
-typedef struct{
-	uint8_t Wakeup: 2;
-	uint8_t Sleep: 1;
-	uint8_t Measure: 1;
-	uint8_t AUTO_SLEEP: 1;
-	uint8_t Link: 1;
-	uint8_t Reserved: 2;
+typedef enum {
+	ADXL345_OK = 0, ADXL345_ERROR, ADXL345_INVALID_ID, ADXL345_INVALID_PARAM
+} ADXL345_Status_t;
 
-}PowerControlRegister_t;
+typedef struct {
+	I2C_HandleTypeDef *hi2c;
+	uint16_t address;
+	uint8_t range;
+	float scaleFactor;
+} ADXL345_Handle_t;
 
-typedef struct{
-	uint8_t Range: 2;
-	uint8_t Justify: 1;
-	uint8_t FULL_RES: 1;
-	uint8_t Reserved: 1;
-	uint8_t INT_INVERT: 1;
-	uint8_t SPI: 1;
-	uint8_t SELF_TEST: 1;
+typedef struct {
+	int16_t x;
+	int16_t y;
+	int16_t z;
+} ADXL345_RawData_t;
 
-}DataFormatRegister_t;
+typedef struct {
+	float x;
+	float y;
+	float z;
+} ADXL345_GData_t;
 
-typedef struct{
-	uint8_t Range: 4;
-	uint8_t LOW_POWER: 1;
-	uint8_t Reserved: 3;
+typedef struct {
+	float roll;
+	float pitch;
+} ADXL345_Angle_t;
 
-}BWRATERegister_t;
-
-int ADXL345_ScanDeviceID(void);
-ADXL345InitStatus ADXL345_Init(void);
-ADXL345ReadStatus ADXL345_ReadRegisterData(uint16_t registerAddress, uint16_t sizeofData, uint8_t *dataBuffer);
-ADXL345WriteStatus ADXL345_WriteRegisterData(uint16_t registerAddress, uint16_t value);
-int16_t ADXL345_getAxisValue(uint8_t axis);
+ADXL345_Status_t ADXL345_Init(ADXL345_Handle_t *dev, I2C_HandleTypeDef *hi2c, uint16_t address);
+ADXL345_Status_t ADXL345_ReadDeviceID(ADXL345_Handle_t *dev, uint8_t *deviceID);
+ADXL345_Status_t ADXL345_IsDeviceReady(ADXL345_Handle_t *dev);
+ADXL345_Status_t ADXL345_SetStandbyMode(ADXL345_Handle_t *dev);
+ADXL345_Status_t ADXL345_SetMeasureMode(ADXL345_Handle_t *dev);
+ADXL345_Status_t ADXL345_SetRange(ADXL345_Handle_t *dev, uint8_t range, uint8_t fullResolution);
+ADXL345_Status_t ADXL345_SetDataRate(ADXL345_Handle_t *dev, uint8_t dataRate, uint8_t lowPower);
+ADXL345_Status_t ADXL345_ReadRawXYZ(ADXL345_Handle_t *dev, ADXL345_RawData_t *rawData);
+ADXL345_Status_t ADXL345_ReadGXYZ(ADXL345_Handle_t *dev, ADXL345_GData_t *gData);
+ADXL345_Status_t ADXL345_CalculateRollPitch(ADXL345_Handle_t *dev, ADXL345_Angle_t *angle);
 
 #endif /* INC_ADXL345_H_ */
--- Core/Src/adxl345.c
+++ Core/Src/adxl345.c
@@ -6,94 +6,273 @@
  */
 
 #include "adxl345.h"
+#include <math.h>
 
-extern I2C_HandleTypeDef hi2c1;
-
-int ADXL345_ScanDeviceID(){
-    for (uint8_t address = 0; address < 255; address++) {
-        if (HAL_I2C_IsDeviceReady(&hi2c1, address, 1, TIMEOUT) == HAL_OK){
-            return address;
-        }
-    }
-
-    return -1;
-}
-
-ADXL345ReadStatus ADXL345_ReadRegisterData(uint16_t registerAddress, uint16_t sizeofData, uint8_t *dataBuffer){
-	if (HAL_I2C_Mem_Read(&hi2c1, ADXL345_DEVICE_ADDRESS, registerAddress, 1, dataBuffer, sizeofData, TIMEOUT) == HAL_OK) {
-		return READ_SUCCESS;
+static ADXL345_Status_t ADXL345_ReadRegister(ADXL345_Handle_t *dev, uint8_t reg,
+		uint8_t *data, uint16_t length) {
+	if (dev == NULL || dev->hi2c == NULL || data == NULL) {
+		return ADXL345_INVALID_PARAM;
 	}
 
-	return READ_FAIL;
-
-}
-
-ADXL345WriteStatus ADXL345_WriteRegisterData(uint16_t registerAddress, uint16_t value){
-	uint8_t data[2] = {0};
-	data[0] = registerAddress;
-	data[1] = value;
-
-	if (HAL_I2C_Master_Transmit(&hi2c1, ADXL345_DEVICE_ADDRESS, data, sizeof(data), TIMEOUT) == HAL_OK) {
-		return WRITE_SUCCESS;
+	if (HAL_I2C_Mem_Read(dev->hi2c, dev->address, reg,
+	I2C_MEMADD_SIZE_8BIT, data, length, ADXL345_TIMEOUT) != HAL_OK) {
+		return ADXL345_ERROR;
 	}
 
-	return WRITE_FAIL;
+	return ADXL345_OK;
 }
 
-ADXL345InitStatus ADXL345_Init(void){
-	uint8_t dataBuffer = 0;
-	ADXL345_ReadRegisterData(DEVID, 1, &dataBuffer);
-
-	if (dataBuffer != 0xE5) {
-		return INIT_FAIL;
+static ADXL345_Status_t ADXL345_WriteRegister(ADXL345_Handle_t *dev,
+		uint8_t reg, uint8_t value) {
+	if (dev == NULL || dev->hi2c == NULL) {
+		return ADXL345_INVALID_PARAM;
 	}
 
-	uint8_t tempReg = 0;
-	PowerControlRegister_t powerControl = {0};
+	if (HAL_I2C_Mem_Write(dev->hi2c, dev->address, reg,
+	I2C_MEMADD_SIZE_8BIT, &value, 1, ADXL345_TIMEOUT) != HAL_OK) {
+		return ADXL345_ERROR;
+	}
 
-	powerControl.Wakeup = WAKEUP_8HZ;
-	powerControl.Sleep = 0x00;
-	powerControl.Measure = 0x01;
-	powerControl.AUTO_SLEEP = 0x00;
-	powerControl.Link = 0x00;
-	powerControl.Reserved = 0x00;
-
-	tempReg = *((uint8_t*)&powerControl);
-
-	ADXL345_WriteRegisterData(POWER_CTL, tempReg);
-
-	DataFormatRegister_t dataFormatControl = {0};
-	dataFormatControl.Range = RANGE_4G;
-	dataFormatControl.Justify = 0x00;
-	dataFormatControl.FULL_RES = 0x00;
-	dataFormatControl.Reserved = 0x00;
-	dataFormatControl.INT_INVERT = 0x00;
-	dataFormatControl.SPI = 0x00;
-	dataFormatControl.SELF_TEST = 0x00;
-
-	tempReg = *((uint8_t*)&dataFormatControl);
-
-	ADXL345_WriteRegisterData(DATA_FORMAT, tempReg);
-
-	BWRATERegister_t BWRATEControl = {0};
-	BWRATEControl.Range = BANDWIDTH_RATE_800;
-	BWRATEControl.LOW_POWER = 0x00;
-	BWRATEControl.Reserved = 0x00;
-
-	tempReg = *((uint8_t*)&BWRATEControl);
-
-	ADXL345_WriteRegisterData(BW_RATE, tempReg);
-
-	return INIT_SUCCESS;
+	return ADXL345_OK;
 }
 
-int16_t ADXL345_getAxisValue(uint8_t axis){
-	uint8_t data[2] = {0};
-	int16_t  outputData = 0;
+ADXL345_Status_t ADXL345_IsDeviceReady(ADXL345_Handle_t *dev) {
+	if (dev == NULL || dev->hi2c == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
 
-	ADXL345_ReadRegisterData(axis, 2, data);
+	if (HAL_I2C_IsDeviceReady(dev->hi2c, dev->address, 3, ADXL345_TIMEOUT)
+			!= HAL_OK) {
+		return ADXL345_ERROR;
+	}
 
-	outputData = ((data[1] << 8) | data[0]);
+	return ADXL345_OK;
+}
 
-	return outputData;
+ADXL345_Status_t ADXL345_ReadDeviceID(ADXL345_Handle_t *dev, uint8_t *deviceID) {
+	if (dev == NULL || deviceID == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+	return ADXL345_ReadRegister(dev, ADXL345_DEVID, deviceID, 1);
+}
+
+ADXL345_Status_t ADXL345_Init(ADXL345_Handle_t *dev, I2C_HandleTypeDef *hi2c,
+		uint16_t address) {
+	uint8_t deviceID = 0;
+
+	if (dev == NULL || hi2c == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	dev->hi2c = hi2c;
+	dev->address = address;
+	dev->range = ADXL345_RANGE_2G;
+	dev->scaleFactor = ADXL345_SCALE_FACTOR_2G;
+
+	if (ADXL345_IsDeviceReady(dev) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	if (ADXL345_ReadDeviceID(dev, &deviceID) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	if (deviceID != ADXL345_DEVICE_ID) {
+		return ADXL345_INVALID_ID;
+	}
+
+	if (ADXL345_SetStandbyMode(dev) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	if (ADXL345_SetRange(dev, ADXL345_RANGE_4G, 0) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	if (ADXL345_SetDataRate(dev, ADXL345_DATARATE_100HZ, 0) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	if (ADXL345_SetMeasureMode(dev) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_SetStandbyMode(ADXL345_Handle_t *dev) {
+	uint8_t powerCtl = 0;
+
+	if (ADXL345_ReadRegister(dev, ADXL345_POWER_CTL, &powerCtl, 1)
+			!= ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	powerCtl &= ~ADXL345_POWER_CTL_MEASURE;
+
+	if (ADXL345_WriteRegister(dev, ADXL345_POWER_CTL, powerCtl) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_SetMeasureMode(ADXL345_Handle_t *dev) {
+	uint8_t powerCtl = 0;
+
+	if (ADXL345_ReadRegister(dev, ADXL345_POWER_CTL, &powerCtl, 1)
+			!= ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	powerCtl |= ADXL345_POWER_CTL_MEASURE;
+
+	if (ADXL345_WriteRegister(dev, ADXL345_POWER_CTL, powerCtl) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_SetRange(ADXL345_Handle_t *dev, uint8_t range,
+		uint8_t fullResolution) {
+	uint8_t dataFormat = 0;
+	float newScaleFactor = dev->scaleFactor;
+
+	if (dev == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	if (range > ADXL345_RANGE_16G) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	if (ADXL345_ReadRegister(dev, ADXL345_DATA_FORMAT, &dataFormat, 1)
+			!= ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	dataFormat &= ~ADXL345_DATA_FORMAT_RANGE_MASK;
+	dataFormat |= range;
+
+	if (fullResolution) {
+		dataFormat |= ADXL345_DATA_FORMAT_FULL_RES;
+		dev->scaleFactor = ADXL345_SCALE_FACTOR_FULL_RES;
+	} else {
+		dataFormat &= ~ADXL345_DATA_FORMAT_FULL_RES;
+
+		switch (range) {
+		case ADXL345_RANGE_2G:
+			dev->scaleFactor = ADXL345_SCALE_FACTOR_2G;
+			break;
+
+		case ADXL345_RANGE_4G:
+			dev->scaleFactor = ADXL345_SCALE_FACTOR_4G;
+			break;
+
+		case ADXL345_RANGE_8G:
+			dev->scaleFactor = ADXL345_SCALE_FACTOR_8G;
+			break;
+
+		case ADXL345_RANGE_16G:
+			dev->scaleFactor = ADXL345_SCALE_FACTOR_16G;
+			break;
+
+		default:
+			return ADXL345_INVALID_PARAM;
+		}
+	}
+
+	if (ADXL345_WriteRegister(dev, ADXL345_DATA_FORMAT, dataFormat)
+			!= ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	dev->range = range;
+	dev->scaleFactor = newScaleFactor;
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_SetDataRate(ADXL345_Handle_t *dev, uint8_t dataRate,
+		uint8_t lowPower) {
+	uint8_t bwRate = 0;
+
+	if (dev == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	if (dataRate > ADXL345_DATARATE_3200HZ) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	bwRate = dataRate & ADXL345_BW_RATE_RATE_MASK;
+
+	if (lowPower) {
+		bwRate |= ADXL345_BW_RATE_LOW_POWER;
+	}
+
+	if (ADXL345_WriteRegister(dev, ADXL345_BW_RATE, bwRate) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_ReadRawXYZ(ADXL345_Handle_t *dev,
+		ADXL345_RawData_t *rawData) {
+	uint8_t data[6] = { 0 };
+
+	if (dev == NULL || rawData == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	if (ADXL345_ReadRegister(dev, ADXL345_DATAX0, data, 6) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	rawData->x = (int16_t) ((data[1] << 8) | data[0]);
+	rawData->y = (int16_t) ((data[3] << 8) | data[2]);
+	rawData->z = (int16_t) ((data[5] << 8) | data[4]);
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_ReadGXYZ(ADXL345_Handle_t *dev, ADXL345_GData_t *gData) {
+	ADXL345_RawData_t rawData = { 0 };
+
+	if (dev == NULL || gData == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	if (ADXL345_ReadRawXYZ(dev, &rawData) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	gData->x = rawData.x * dev->scaleFactor;
+	gData->y = rawData.y * dev->scaleFactor;
+	gData->z = rawData.z * dev->scaleFactor;
+
+	return ADXL345_OK;
+}
+
+ADXL345_Status_t ADXL345_CalculateRollPitch(ADXL345_Handle_t *dev,
+		ADXL345_Angle_t *angle) {
+	ADXL345_GData_t gData = { 0 };
+
+	if (dev == NULL || angle == NULL) {
+		return ADXL345_INVALID_PARAM;
+	}
+
+	if (ADXL345_ReadGXYZ(dev, &gData) != ADXL345_OK) {
+		return ADXL345_ERROR;
+	}
+
+	angle->roll = atan2f(gData.y, gData.z) * 180.0f / 3.14159265f;
+
+	angle->pitch = atan2f(-gData.x,
+			sqrtf((gData.y * gData.y) + (gData.z * gData.z))) * 180.0f
+			/ 3.14159265f;
+
+	return ADXL345_OK;
 }
